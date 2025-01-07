1f3cb121722d7bfe061c5aa4723f6c66ea40f67e
joannachadzynska <jmalek87@gmail.com>
2025-01-07 20:55:03 +0100

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { Button } from "@/components/ui/button"
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <Button>Click me</Button>
+      </div>
+    </>
+  )
+}
+
+export default App
